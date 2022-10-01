f22b2f262c33a8683d28f51c19ffff555f0d5fc1
Creedo11 <keegkree@gmail.com>
2022-10-01 13:27:35 -0400

    created a function in App.js to getAllSongs and ensure the app was connecting with the backend API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [song, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+  
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data)
+    setSongs(response.data)
+  }
+
   return (
     <div>
       Hello World 
